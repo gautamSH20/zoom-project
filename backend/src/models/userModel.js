96eac3b0049b05d3ce1a3e4845215453ca0d80c6
gautamSH20 <gs841400@gmail.com>
2024-12-06 22:11:52 +0530

    typos in the schemas

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -2,9 +2,9 @@ import mongoose, { Schema } from "mongoose";
 
 const userScehma = new Schema({
   name: { type: String, required: true },
-  username: { type: String, required: true, unique: ture },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  token: { typr: String },
+  token: { type: String },
 });
 
 const User = mongoose.model("User", userScehma);
