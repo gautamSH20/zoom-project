1a87593338c2a6586e28b2e2d7788cc79d6b7cfd
gautamSH20 <gs841400@gmail.com>
2024-12-06 22:09:41 +0530

    Intialzed first register and login

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,63 @@
+import { User } from "../models/userModel.js";
+import httpStatus from "http-status";
+import bcrypt, { hash } from "bcrypt";
+import crypto from "crypto";
+
+// REGISTER
+const register = async (req, res) => {
+  const { name, username, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      res.status(httpStatus.FOUND).json({ message: "User already exsist" });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: name,
+      username: username,
+      password: hashPassword,
+    });
+    await newUser.save();
+    res.status(httpStatus.CREATED).json({ message: "Usr created" });
+  } catch (e) {
+    res.json({
+      message: `Someting went wrong${e}`,
+    });
+  }
+};
+
+// LOGIN
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Please Provide valid info" });
+  }
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "User Not Found" });
+    }
+    if (bcrypt.compare(password, user.password)) {
+      let token = crypto.randomBytes(20).toString("hex");
+      user.token = token;
+      await user.save();
+      //   setTimeout(async () => {
+      //     try {
+      //       user.token = null;
+      //       await user.save();
+      //       console.log("token has been deleted ");
+      //     } catch (e) {
+      //       console.log(`something went wrong ${e}`);
+      //     }
+      //   }, 2000);
+      return res.status(httpStatus.OK).json({ token: token });
+    }
+  } catch (e) {
+    return res.status(500).json({ message: `something went wrong ${e}` });
+  }
+};
+
+export { register, login };
