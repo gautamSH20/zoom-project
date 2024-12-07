6193822bcf91a3fd371d94709f8253ae0871381c
gautamSH20 <gs841400@gmail.com>
2024-12-07 18:51:29 +0530

    adding socket connections

--- backend/src/controllers/socketManger.js
+++ backend/src/controllers/socketManger.js
@@ -1,5 +1,16 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+let connections = {};
+let messages = {};
+let timeOnline = {};
 export const connectTo = (server) => {
   const io = new Server(server);
+  io.on("connection", (socket) => {
+    socket.on("join-call", (path) => {});
+    socket.on("signal", (toId, message) => {
+      io.to(toId).emit("signal", socket.id, message);
+    });
+    socket.on("chat-message", (data, sender) => {});
+    socket.on("disconnect", () => {});
+  });
   return io;
 };
