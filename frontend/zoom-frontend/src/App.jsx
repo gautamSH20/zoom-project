f1a16f9c8bc95f7002b855a1106e252718c31037
gautamSH20 <gs841400@gmail.com>
2024-12-11 19:44:07 +0530

    adding auth route and AuthContetxt-Provider

--- frontend/zoom-frontend/src/App.jsx
+++ frontend/zoom-frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing.jsx";
+import Authentication from "./pages/authentication.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/home" element={<LandingPage />} />
+            <Route path="/auth" element={<Authentication />} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </>
   );
