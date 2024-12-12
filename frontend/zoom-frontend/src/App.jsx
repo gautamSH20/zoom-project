de3c82fcb3617fc9e9198cc413d0da657f5be9fb
gautamSH20 <gs841400@gmail.com>
2024-12-12 22:57:09 +0530

    nothing changed

--- frontend/zoom-frontend/src/App.jsx
+++ frontend/zoom-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing.jsx";
 import Authentication from "./pages/authentication.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
