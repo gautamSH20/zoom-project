ddeaf0a0aac185a18474c22b584c85cb535a755a
gautamSH20 <gs841400@gmail.com>
2024-12-12 23:02:26 +0530

    snackbar added handel auth added

--- frontend/zoom-frontend/src/pages/authentication.jsx
+++ frontend/zoom-frontend/src/pages/authentication.jsx
@@ -3,16 +3,14 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
+
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
+
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext.jsx";
 import { Snackbar } from "@mui/material";
 
 // TODO remove, this demo shouldn't need to reset the theme.
@@ -20,10 +18,10 @@ import { Snackbar } from "@mui/material";
 const defaultTheme = createTheme();
 
 export default function Authentication() {
-  const [username, setUsername] = React.useState();
-  const [password, setPassword] = React.useState();
-  const [name, setName] = React.useState();
-  const [error, setError] = React.useState();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [error, setError] = React.useState("");
   const [message, setMessage] = React.useState();
 
   const [formState, setFormState] = React.useState(0);
@@ -32,7 +30,7 @@ export default function Authentication() {
 
   const { handleRegister, handleLogin } = React.useContext(AuthContext);
 
-  let handleAuth = async () => {
+  const handleAuth = async () => {
     try {
       if (formState === 0) {
         let result = await handleLogin(username, password);
@@ -41,6 +39,7 @@ export default function Authentication() {
         let result = await handleRegister(name, username, password);
         console.log(result);
         setUsername("");
+        setName("");
         setMessage(result);
         setOpen(true);
         setError("");
@@ -48,9 +47,8 @@ export default function Authentication() {
         setPassword("");
       }
     } catch (err) {
-      console.log(err);
-      let message = err.response.data.message;
-      setError(message);
+      let messages = err.response.data.message;
+      setError(messages);
     }
   };
 
@@ -164,7 +162,7 @@ export default function Authentication() {
         </Grid>
       </Grid>
 
-      <Snackbar open={open} autoHideDuration={4000} message={message} />
+      <Snackbar open={open} autoHideDuration={400} message={message}></Snackbar>
     </ThemeProvider>
   );
 }
