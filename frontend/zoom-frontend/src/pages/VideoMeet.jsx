92d98c7a040cd4574455ac30a18784c7bbd8a164
gautamSH20 <gs841400@gmail.com>
2024-12-19 21:40:45 +0530

    build and rtc connection for video streaming

--- frontend/zoom-frontend/src/pages/VideoMeet.jsx
+++ frontend/zoom-frontend/src/pages/VideoMeet.jsx
@@ -18,7 +18,7 @@ export default function VideoMeet() {
   let localVideoRef = useRef();
   let [videoAvailabel, setVideoAvailabel] = useState(true);
   let [audioAvailable, setAudioAvailable] = useState(true);
-  let [video, setVideo] = useState();
+  let [video, setVideo] = useState([]);
   let [audio, setAudio] = useState();
   let [screen, setscreen] = useState();
   let [showModel, setModel] = useState();
@@ -107,10 +107,97 @@ export default function VideoMeet() {
   }, [audio, video]);
 
   let gotMessageFromServer = () => {};
+  let addMessage = () => {};
 
   let connectToSocket = () => {
     socketRef.current = io.connect(server_url, { secure: false });
-    socketIdRef.current.on("signal", gotMessageFromServer);
+    socketRef.current.on("signal", gotMessageFromServer);
+    socketRef.current.on("connect", () => {
+      socketRef.current.emit("join call", window.location.href);
+      socketIdRef.current = socketRef.current.id;
+      socketRef.current.on("chat-message", addMessage);
+
+      socketRef.current.on("user-left", (id) => {
+        setVideo((videos) => videos.filter((video) => video.socketId !== id));
+      });
+      socketRef.current.on("user-joinde", (id, client) => {
+        client.forEach((socketListId) => {
+          connections[socketListId] = new RTCPeerConnection(
+            peerConfigConnections
+          );
+          connections[socketListId].onicecandidate = (event) => {
+            if (event.candidate != null) {
+              socketRef.current.emit(
+                "signal",
+                socketListId,
+                JSON.stringify({ ice: event.candidate })
+              );
+            }
+          };
+          // ADDING STREAM
+          connections[socketListId].onaddstream = (event) => {
+            let videoExists = videoRef.current.find(
+              (video) => video.socketId === socketListId
+            );
+
+            if (videoExists) {
+              setVideo((videos) => {
+                const updatedVide = videos.map((video) => {
+                  video.socketId === socketListId
+                    ? { ...video, stream: event.stream }
+                    : video;
+                });
+                videoRef.current = updatedVide;
+                return updatedVide;
+              });
+            } else {
+              let newVid = {
+                socketId: socketListId,
+                stream: event.stream,
+                autoPlay: true,
+                playsinline: true,
+              };
+
+              setVideo((video) => {
+                const updatedVide = [...video, newVid];
+                videoRef.current = updatedVide;
+                return updatedVide;
+              });
+            }
+          };
+          // AADING THE LOCAL VIDEO STREAM
+          if (window.localStream !== undefined && window.localStream !== null) {
+            connections[socketListId].addStream(window.localStream);
+          } else {
+            //TODO BLACK-SILENCE
+            // let black-silence
+          }
+          if (id === socketIdRef.current) {
+            for (let id2 in connections) {
+              if (id2 === socketIdRef) continue;
+
+              try {
+                connections[id2].addStream(window.localStream);
+              } catch (e) {
+                console.log(e);
+              }
+              connections[id2].createOffer().then((description) => {
+                connections[id2]
+                  .setLocalDescription(description)
+                  .then(() => {
+                    socketRef.current.emit(
+                      "signal",
+                      id2,
+                      JSON.stringify({ sdp: connections[id2].localStream })
+                    );
+                  })
+                  .catch((e) => console.log(e));
+              });
+            }
+          }
+        });
+      });
+    });
   };
 
   let getMedia = () => {
