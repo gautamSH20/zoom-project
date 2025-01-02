b13cff6ac76ac06d083d6e34696bd20d87b5c9f4
gautamSH20 <gs841400@gmail.com>
2025-01-02 12:01:46 +0530

    old routes removed

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,7 +4,5 @@ const router = Router();
 
 router.route("/login").post(login);
 router.route("/register").post(register);
-router.route("/add_to_activity");
-router.route("/get_all_activity");
 
 export default router;
